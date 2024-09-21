b01312d56c4b8dd42cf6a4e060f41071527b0da8
RKrava <krab9887@gmail.com>
2024-09-21 12:54:53 +0300

    platizhka fix brick

--- index.js
+++ index.js
@@ -110,6 +110,10 @@ const getCartShopify = async (cartId, storeId) => {
                   }
                 }
               }
+              attributes {
+                key
+                value
+              }
             }
           }
         }
